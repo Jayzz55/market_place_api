94d6ca401853f3d55133ca5c70855b6490e5c193
Jayzz55 <jayzzwijono@yahoo.com>
2014-11-30 06:59:28 +0000

    Adds user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :auth_token, uniqueness: true
+
+  before_create :generate_authentication_token!
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+      #alternatively use: self.auth_token = SecureRandom.hex
+    end while self.class.exists?(auth_token: auth_token)
+  end
+  
 end
